ef43f59212da8525bf3d04094641cf8fb6c208af
Victor Kinyanjui <leslievictorius@gmail.com>
2025-03-19 13:39:16 +0000

    Configured mongodb cluster and painfully connected to it using mongoode

--- myApp.js
+++ myApp.js
@@ -1,5 +1,27 @@
 require('dotenv').config();
 
+// console.log(process.env.MONGO_URI);
+
+/** # MONGOOSE SETUP #
+/*  ================== */
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+mongoose.connect(uri, clientOptions);
+
+// async function run() {
+//   try {
+//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+//     await mongoose.connect(uri, clientOptions);
+//     await mongoose.connection.db.admin().command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await mongoose.disconnect();
+//   }
+// }
+// run().catch(console.dir);
 
 let Person;
 
