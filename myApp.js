da931e7b1ddad3d850dc8d9c763e169f4a12eb38
Victor Kinyanjui <leslievictorius@gmail.com>
2025-03-20 13:22:39 +0000

    Specified db-name in connection uri bypassing permissions ia new ssue with admin db; created a mongoose schema and model

--- myApp.js
+++ myApp.js
@@ -5,29 +5,70 @@ require('dotenv').config();
 /** # MONGOOSE SETUP #
 /*  ================== */
 const mongoose = require('mongoose');
+
+// Like
+// MONGO_URI=mongodb+srv://<user>:<db-password>@<cluster-name>.d2ylz.mongodb.net/<db-name>?retryWrites=true&w=majority&appName=<cluster-name>
+// The database name can also be specified (without a trailing /)
+// To avoid connecting to the admin database by default
+// for which we may not have the necessary permissions
+// Ensure useNewUrlParser: true is specified in mongoose.connect
 const uri = process.env.MONGO_URI;
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-mongoose.connect(uri, clientOptions);
+const clientOptions = { useNewUrlParser: true, serverApi: { version: '1', strict: true, deprecationErrors: true } };
+// mongoose.connect(uri, clientOptions);
 
-// async function run() {
-//   try {
-//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-//     await mongoose.connect(uri, clientOptions);
-//     await mongoose.connection.db.admin().command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await mongoose.disconnect();
-//   }
-// }
+
+let personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+})
+
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
 // run().catch(console.dir);
 
-let Person;
+class Database {
+  constructor() {
+    this._connect();
+  }
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  _connect() {
+    mongoose
+      .connect(uri, clientOptions)
+      .then(() => {
+        console.log('Database connection successful');
+      })
+      .catch((err) => {
+        console.error('Database connection error');
+      });
+  }
+}
+
+let database = new Database();
+
+let Person = new mongoose.model("Person", personSchema);
+
+let createAndSavePerson = function(done) {
+  let janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
+
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
